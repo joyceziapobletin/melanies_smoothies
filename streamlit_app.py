46453706b8e9676e9a0186cefdcd2ec228c3a74e
joyceziapobletin <joyce.zia.pobletin@accenture.com>
2024-10-21 22:25:05 +0800

    Update streamlit_app.py
    connected changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 ingredients_list = st.multiselect(
